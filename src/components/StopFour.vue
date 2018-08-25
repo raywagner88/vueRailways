6aad89230886f504cef7f75a5e29def58d4167ad
Ray Wagner <raywagner88@me.com>
2018-08-25 11:05:06 -0500

    Update api for stops three and four

--- src/components/StopFour.vue
+++ src/components/StopFour.vue
@@ -36,7 +36,7 @@ export default {
   },
   mounted () {
     axios
-      .get('https://api.fremonttrains.com/crossings/2/status', { crossdomain: true })
+      .get('https://api.fremonttrains.com/crossings/4/status', { crossdomain: true })
       .then(response => {
         this.info = response.data
       })
--- src/components/StopThree.vue
+++ src/components/StopThree.vue
@@ -36,7 +36,7 @@ export default {
   },
   mounted () {
     axios
-      .get('https://api.fremonttrains.com/crossings/2/status', { crossdomain: true })
+      .get('https://api.fremonttrains.com/crossings/3/status', { crossdomain: true })
       .then(response => {
         this.info = response.data
       })
