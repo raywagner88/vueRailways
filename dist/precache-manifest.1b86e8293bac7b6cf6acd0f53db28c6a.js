049b356626aa16d41f19b019097a41ea1bfa6949
Ray Wagner <raywagner88@me.com>
2018-08-24 20:52:31 -0500

    Add dist file

---
+++ dist/precache-manifest.1b86e8293bac7b6cf6acd0f53db28c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "eafbe0878017c4f82178",
+    "url": "/js/chunk-vendors.31ee1662.js"
+  },
+  {
+    "revision": "da16d29e7dec9ff9d7cd",
+    "url": "/js/app.d4d208a1.js"
+  },
+  {
+    "revision": "017a8e1d13420f2d375168483e9d9e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "da16d29e7dec9ff9d7cd",
+    "url": "/css/app.6de8c8fb.css"
+  }
+];
