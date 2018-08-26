f35e73bcb276ba2d5a66839dc6a7cec81f624a82
Ray Wagner <raywagner88@me.com>
2018-08-25 23:17:16 -0500

    Update .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'eol-last': 'error',
+    'no-var': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': 'error',
+    'comma-dangle': ['error', 'never'],
+    'quotes': ['error', 'single']
   },
   parserOptions: {
     parser: 'babel-eslint'
